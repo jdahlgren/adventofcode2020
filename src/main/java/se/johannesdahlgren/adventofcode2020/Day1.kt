c55ec97f87cb99753426266c5c2a006e962b1edd
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2020-12-01 21:05:14 +0100

    Day1 refactor

--- src/main/java/se/johannesdahlgren/adventofcode2020/Day1.kt
+++ src/main/java/se/johannesdahlgren/adventofcode2020/Day1.kt
@@ -5,37 +5,26 @@ class Day1(fileName: String) {
     private val input: List<Long> = readFileToLongList(fileName)
 
     fun fixExpenseReport(sum: Long): Long {
-        val factors = findSum(input, sum)
-        return factors.stream().reduce(Long::times).get()
-    }
-
-    fun fixExpenseReportThree(sum: Long): Long {
-        val factors = findSumThree(input, sum)
-        return factors.stream().reduce(Long::times).get()
-    }
-
-    private fun findSum(input: List<Long>, sum: Long): List<Long> {
         for (i in input) {
             for (j in input) {
                 if (i + j == sum) {
-                    return listOf(i, j)
+                    return i * j
                 }
             }
         }
         throw Exception("Input does not contain items that sum to $sum")
     }
 
-    private fun findSumThree(input: List<Long>, sum: Long): List<Long> {
+    fun fixExpenseReportThree(sum: Long): Long {
         for (i in input) {
             for (j in input) {
                 for (k in input) {
                     if (i + j + k == sum) {
-                        return listOf(i, j, k)
+                        return i * j * k
                     }
                 }
             }
         }
         throw Exception("Input does not contain items that sum to $sum")
     }
-
 }
