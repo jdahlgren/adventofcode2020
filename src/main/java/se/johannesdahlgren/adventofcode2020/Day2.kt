4a19c17adca754b66768c450c649304490ba7599
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2020-12-02 21:36:39 +0100

    Day2 Part2, Trim the password!

--- src/main/java/se/johannesdahlgren/adventofcode2020/Day2.kt
+++ src/main/java/se/johannesdahlgren/adventofcode2020/Day2.kt
@@ -4,21 +4,29 @@ class Day2(fileName: String) {
     private val input = readFileToStringList(fileName)
     private var correctPasswords = 0
 
-    fun getCorrectPasswords(): Int {
-
+    fun getCorrectPasswords(system: String): Int {
         input.forEach { s ->
             val policyPassword = s.split(":")
             val minMaxChar = policyPassword[0].split(" ")
-            val password = policyPassword[1]
+            val password = policyPassword[1].trim()
             val minMax = minMaxChar[0].split("-")
             val key = minMaxChar[1][0]
             val min = minMax[0].toInt()
             val max = minMax[1].toInt()
 
-            val count = password.count { key == it }
-            if (count in min..max) {
-                correctPasswords++
+            if(system == "sledRental") {
+                val count = password.count { key == it }
+                if (count in min..max) {
+                    correctPasswords++
+                }
             }
+
+            if(system == "tobogganCorp") {
+                if ((password[min - 1] == key).xor(password[max - 1] == key)) {
+                    correctPasswords++
+                }
+            }
+
         }
 
         return correctPasswords
--- src/test/java/se/johannesdahlgren/adventofcode2020/Day2Test.kt
+++ src/test/java/se/johannesdahlgren/adventofcode2020/Day2Test.kt
@@ -9,7 +9,7 @@ internal class Day2Test {
     fun part1Example() {
         val day2 = Day2("day2_example.txt")
 
-        val numberOfCorrectPasswords = day2.getCorrectPasswords()
+        val numberOfCorrectPasswords = day2.getCorrectPasswords("sledRental")
         assertEquals(2, numberOfCorrectPasswords)
     }
 
@@ -17,7 +17,23 @@ internal class Day2Test {
     fun part1() {
         val day2 = Day2("day2.txt")
 
-        val numberOfCorrectPasswords = day2.getCorrectPasswords()
+        val numberOfCorrectPasswords = day2.getCorrectPasswords("sledRental")
         assertEquals(640, numberOfCorrectPasswords)
+    }
+
+    @Test
+    fun part2Example() {
+        val day2 = Day2("day2_example.txt")
+
+        val numberOfCorrectPasswords = day2.getCorrectPasswords("tobogganCorp")
+        assertEquals(1, numberOfCorrectPasswords)
+    }
+
+    @Test
+    fun part2() {
+        val day2 = Day2("day2.txt")
+
+        val numberOfCorrectPasswords = day2.getCorrectPasswords("tobogganCorp")
+        assertEquals(472, numberOfCorrectPasswords)
     }
 }
