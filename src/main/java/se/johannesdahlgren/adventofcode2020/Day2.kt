f5269afd84f6bcbfca6f32d96cb8ee4163853dc5
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2020-12-02 21:19:28 +0100

    Day2 Part1, skip the streams

--- src/main/java/se/johannesdahlgren/adventofcode2020/Day2.kt
+++ src/main/java/se/johannesdahlgren/adventofcode2020/Day2.kt
@@ -1,37 +1,26 @@
 package se.johannesdahlgren.adventofcode2020
 
-import kotlin.streams.toList
-
 class Day2(fileName: String) {
     private val input = readFileToStringList(fileName)
     private var correctPasswords = 0
 
     fun getCorrectPasswords(): Int {
 
-        val test = input.stream()
-            .map { it.split(Regex(":")) }
-            .map { it[0] to it[1] }
-            .map { it.first.split(Regex(" ")) to it.second}
-            .map { it.first[0] to it.first[1] to it.second }
-            .map { val split = it.first.first.split(Regex("-"))
-                split[0] to split[1] to it.first.second to it.second
-            }
-            .toList()
+        input.forEach { s ->
+            val policyPassword = s.split(":")
+            val minMaxChar = policyPassword[0].split(" ")
+            val password = policyPassword[1]
+            val minMax = minMaxChar[0].split("-")
+            val key = minMaxChar[1][0]
+            val min = minMax[0].toInt()
+            val max = minMax[1].toInt()
 
-        test.forEach { pair ->
-            val min = pair.first.first.first.toInt()
-            val max = pair.first.first.second.toInt()
-            val key = pair.first.second
-            val password = pair.second
-
-            val count = password.count { key.contains(it) }
-            if(count in min..max) {
+            val count = password.count { key == it }
+            if (count in min..max) {
                 correctPasswords++
             }
         }
 
         return correctPasswords
     }
-
-
 }
