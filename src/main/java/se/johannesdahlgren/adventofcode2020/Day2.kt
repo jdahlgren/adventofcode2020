1aa24f389cf58d985db95f4f50037ed97f15237f
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2020-12-02 22:46:00 +0100

    Day2 Refactor to streams again. Remove counters that the stream can do for us

--- src/main/java/se/johannesdahlgren/adventofcode2020/Day2.kt
+++ src/main/java/se/johannesdahlgren/adventofcode2020/Day2.kt
@@ -1,38 +1,28 @@
 package se.johannesdahlgren.adventofcode2020
 
-class Day2(fileName: String) {
-    private val input = readFileToStringList(fileName)
-    private var correctPasswords = 0
+class Day2(private val fileName: String) {
+    private data class Holder<A, B, C>(val a: A, val b: A, val c: B, val d: C)
 
-    fun getCorrectPasswordsSledRental(): Int {
-        input.forEach { s ->
-            val splits = splitInputByRegex(s)
-            val (_, min, max, key, password) = splits.destructured
-
-            val count = password.count { key[0] == it }
-            if (count in min.toInt()..max.toInt()) {
-                correctPasswords++
+    fun getCorrectPasswordsSledRental(): Long {
+        return readFileToStringList(fileName).stream()
+            .map { regexToHolder(it) }
+            .filter { (min, max, key, password) ->
+                password.count { it == key } in min..max
             }
-        }
-        return correctPasswords
+            .count()
     }
 
-    fun getCorrectPasswordsTobogganCorp(): Int {
-        input.forEach { s ->
-            val splits = splitInputByRegex(s)
-            val (_, min, max, key, password) = splits.destructured
-
-            val matchPos1 = password[min.toInt() - 1] == key[0]
-            val matchPos2 = password[max.toInt() - 1] == key[0]
-            if (matchPos1.xor(matchPos2)) {
-                correctPasswords++
+    fun getCorrectPasswordsTobogganCorp(): Long {
+        return readFileToStringList(fileName).stream()
+            .map { regexToHolder(it) }
+            .filter { (min, max, key, password) ->
+                (password[min - 1] == key).xor(password[max - 1] == key)
             }
-        }
-        return correctPasswords
+            .count()
     }
 
-    private fun splitInputByRegex(s: String): MatchResult {
-        val regex = Regex("((\\d+)-(\\d+) (\\S): (.*))")
-        return regex.find(s)!!
+    private fun regexToHolder(line: String): Holder<Int, Char, String> {
+        val (_, min, max, key, password) = "((\\d+)-(\\d+) (\\S): (.*))".toRegex().find(line)!!.destructured
+        return Holder(min.toInt(), max.toInt(), key[0], password)
     }
 }
