48c603eaaac8c654a2ee1bd50b20108ea9aa044c
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2020-12-03 22:04:13 +0100

    Day3 prepare for part2

--- src/main/java/se/johannesdahlgren/adventofcode2020/Day3.kt
+++ src/main/java/se/johannesdahlgren/adventofcode2020/Day3.kt
@@ -1,20 +1,20 @@
 package se.johannesdahlgren.adventofcode2020
 
 private const val TREE = '#'
-private const val STEP_DOWN = 3
 
 class Day3(fileName: String) {
-    private var trees = 0
 
     private val map = readFileTo2DArray(fileName)
 
-    fun countTrees(): Int {
+    fun countTrees(stepRight: Int, stepDown: Int): Int {
+        var trees = 0
         var col = 0
-        for (row in map) {
+        for (x in map.indices step stepDown) {
+            val row = map[x]
             if (row[col] == TREE) {
                 trees++
             }
-            col += STEP_DOWN
+            col += stepRight
             if (col > row.size - 1) {
                 col = 0 + col % row.size
             }
--- src/test/java/se/johannesdahlgren/adventofcode2020/Day3Test.kt
+++ src/test/java/se/johannesdahlgren/adventofcode2020/Day3Test.kt
@@ -8,14 +8,14 @@ internal class Day3Test {
     @Test
     fun part1Example() {
         val day3 = Day3("day3_example.txt")
-        val trees = day3.countTrees()
+        val trees = day3.countTrees(3, 1)
         assertEquals(7, trees)
     }
 
     @Test
     fun part1() {
         val day3 = Day3("day3.txt")
-        val trees = day3.countTrees()
+        val trees = day3.countTrees(3, 1)
         assertEquals(207, trees)
     }
 }
