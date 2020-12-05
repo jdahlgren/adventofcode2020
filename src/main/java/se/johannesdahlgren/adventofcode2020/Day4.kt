a27cb706e6ee4c9f90a8bf814a77f61e6f83c0a2
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2020-12-05 14:32:43 +0100

    Day4 Part2: Completed

--- src/main/java/se/johannesdahlgren/adventofcode2020/Day4.kt
+++ src/main/java/se/johannesdahlgren/adventofcode2020/Day4.kt
@@ -7,21 +7,44 @@ class Day4(private val fileName: String) {
     private val blankLinePattern = "^\\s*\$"
     private val mandatoryFields = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
 
-    fun getValidPassports(): Long {
-        val batchFile = readFileToString(fileName)
-        val passPorts = batchFile.split(Regex(blankLinePattern, RegexOption.MULTILINE))
+    fun getPassportsWithRequiredFields(): Long {
+        val passPorts = getPassportsFromFile()
 
         return passPorts.stream()
             .map { getPassportKeys(it) }
             .filter { it.containsAll(mandatoryFields) }
             .count()
-
     }
+
+    fun getValidPassports(): Long {
+        val passPorts = getPassportsFromFile()
+
+        return passPorts.stream()
+            .filter { hasCorrectValues(it) }
+            .count()
+    }
+
+    private fun hasCorrectValues(passPort: String): Boolean {
+        val validByr = "byr:(19[2-9]\\d|200[0-2])".toRegex().containsMatchIn(passPort)
+        val validIyr = "iyr:(201\\d|2020)".toRegex().containsMatchIn(passPort)
+        val validEyr = "eyr:(202\\d|2030)".toRegex().containsMatchIn(passPort)
+        val validHgt = "hgt:((1([5-8]\\d|9[0-3])cm)|((59|(6\\d)|(7[0-6]))in))".toRegex().containsMatchIn(passPort)
+        val validHcl = "hcl:#[0-9|a-f]{6}".toRegex().containsMatchIn(passPort)
+        val validEcl = "ecl:(amb|blu|brn|gry|grn|hzl|oth)".toRegex().containsMatchIn(passPort)
+        val validPid = "pid:\\d{9}\\b".toRegex().containsMatchIn(passPort)
+        return validByr && validIyr && validEyr && validHcl && validEcl && validPid && validHgt
+    }
+
 
     private fun getPassportKeys(passPort: String): List<String> {
         val matchPassportKeys = "(.{3}):".toRegex().findAll(passPort).toList()
-        return matchPassportKeys.stream ()
+        return matchPassportKeys.stream()
             .map { it.groupValues[1] }
             .toList()
+    }
+
+    private fun getPassportsFromFile(): List<String> {
+        val batchFile = readFileToString(fileName)
+        return batchFile.split(Regex(blankLinePattern, RegexOption.MULTILINE))
     }
 }
--- src/test/java/se/johannesdahlgren/adventofcode2020/Day4Test.kt
+++ src/test/java/se/johannesdahlgren/adventofcode2020/Day4Test.kt
@@ -8,14 +8,35 @@ internal class Day4Test {
     @Test
     fun part1Example() {
         val day4 = Day4("day4_example.txt")
-        val validPassports = day4.getValidPassports()
+        val validPassports = day4.getPassportsWithRequiredFields()
         assertEquals(2, validPassports)
     }
 
     @Test
     fun part1() {
         val day4 = Day4("day4.txt")
-        val validPassports = day4.getValidPassports()
+        val validPassports = day4.getPassportsWithRequiredFields()
         assertEquals(170, validPassports)
+    }
+
+    @Test
+    fun part2InvalidExample() {
+        val day4 = Day4("day4_part2_invalid_example.txt")
+        val validPassports = day4.getValidPassports()
+        assertEquals(0, validPassports)
+    }
+
+    @Test
+    fun part2ValidExample() {
+        val day4 = Day4("day4_part2_valid_example.txt")
+        val validPassports = day4.getValidPassports()
+        assertEquals(4, validPassports)
+    }
+
+    @Test
+    fun part2() {
+        val day4 = Day4("day4.txt")
+        val validPassports = day4.getValidPassports()
+        assertEquals(103, validPassports)
     }
 }
