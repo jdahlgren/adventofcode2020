d7b7ada1d12f2e05fce8b9312eb6fab0f0275292
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2020-12-06 18:13:46 +0100

    Day6 part2 solved!

--- src/main/java/se/johannesdahlgren/adventofcode2020/Day6.kt
+++ src/main/java/se/johannesdahlgren/adventofcode2020/Day6.kt
@@ -1,7 +1,7 @@
 package se.johannesdahlgren.adventofcode2020
 
 class Day6 {
-    fun sumYes(fileName: String): Int {
+    fun sumAnyoneYes(fileName: String): Int {
         val answersFromGroups = readFileToStringListSplitEmptyLine(fileName)
         return answersFromGroups.stream()
             .map { it.replace("\r\n", "") }
@@ -10,5 +10,21 @@ class Day6 {
             .map { it.count() }
             .reduce { a, b -> a + b }
             .orElse(-1)
+    }
+
+    fun sumEveryoneYes(fileName: String): Int {
+        val answersFromGroups = readFileToStringListSplitEmptyLine(fileName)
+        var count = 0
+        for (answersFromGroup in answersFromGroups) {
+            val answersFromPersonsInGroup = answersFromGroup.trim()
+                .split(Regex("\r\n", RegexOption.MULTILINE))
+            var mutableSetOf = answersFromPersonsInGroup[0].toCharArray().toSet()
+            for (answersFromPerson in answersFromPersonsInGroup) {
+                mutableSetOf = answersFromPerson.toCharArray().intersect(mutableSetOf)
+            }
+            count += mutableSetOf.count()
+        }
+
+        return count
     }
 }
--- src/test/java/se/johannesdahlgren/adventofcode2020/Day6Test.kt
+++ src/test/java/se/johannesdahlgren/adventofcode2020/Day6Test.kt
@@ -8,14 +8,28 @@ internal class Day6Test {
     @Test
     fun part1Example() {
         val day6 = Day6()
-        val sumYeses = day6.sumYes("day6_example")
+        val sumYeses = day6.sumAnyoneYes("day6_example")
         assertEquals(11, sumYeses)
     }
 
     @Test
     fun part1() {
         val day6 = Day6()
-        val sumYeses = day6.sumYes("day6")
+        val sumYeses = day6.sumAnyoneYes("day6")
         assertEquals(7128, sumYeses)
+    }
+
+    @Test
+    fun part2Example() {
+        val day6 = Day6()
+        val sumYeses = day6.sumEveryoneYes("day6_example")
+        assertEquals(6, sumYeses)
+    }
+
+    @Test
+    fun part2() {
+        val day6 = Day6()
+        val sumYeses = day6.sumEveryoneYes("day6")
+        assertEquals(3640, sumYeses)
     }
 }
