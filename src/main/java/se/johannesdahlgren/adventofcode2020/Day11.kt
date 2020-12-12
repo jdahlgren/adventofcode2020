abf00e4b2acef636ceb8f3eae8200fc596d8e0f6
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2020-12-12 18:50:10 +0100

    Day11 part1 Solved

---
+++ src/main/java/se/johannesdahlgren/adventofcode2020/Day11.kt
@@ -0,0 +1,55 @@
+package se.johannesdahlgren.adventofcode2020
+
+class Day11 {
+    fun getOccupiedSeatsWhenStable(input: List<List<Char>>): Int {
+        var seatMap = input.map { it.toCharArray() }.toTypedArray()
+        var seatsChanged = -1
+
+        while (seatsChanged != 0) {
+            seatsChanged = 0
+            val nextSeatMap = seatMap.map { it.copyOf() }.toTypedArray()
+            for (rowIndex in seatMap.indices) {
+                for (colIndex in seatMap[rowIndex].indices) {
+                    val currentSeat = seatMap[rowIndex][colIndex]
+//                    print(currentSeat)
+                    when {
+                        currentSeat == 'L' && hasOccupiedAdjacentSeats(seatMap, rowIndex, colIndex) == 0 -> {
+                            nextSeatMap[rowIndex][colIndex] = '#'
+                            seatsChanged++
+                        }
+                        currentSeat == '#' && hasOccupiedAdjacentSeats(seatMap, rowIndex, colIndex) >= 4 -> {
+                            nextSeatMap[rowIndex][colIndex] = 'L'
+                            seatsChanged++
+                        }
+                    }
+                }
+//                println()
+            }
+            seatMap = nextSeatMap
+//            println()
+        }
+
+
+        return seatMap.map { it.toList() }.toList().flatten().count { it == '#' }
+    }
+
+    private fun hasOccupiedAdjacentSeats(seatMap: Array<CharArray>, currentRow: Int, currentCol: Int): Int {
+        var occupiedAdjacentSeats = 0
+        for (i in -1..1) {
+            for (j in -1..1) {
+                val adjacentRow = currentRow + i
+                val adjacentCol = currentCol + j
+                val currentSeat = adjacentRow == currentRow && adjacentCol == currentCol
+                if (!currentSeat &&
+                    adjacentRow in seatMap.indices &&
+                    adjacentCol in seatMap[currentRow].indices &&
+                    seatMap[adjacentRow][adjacentCol] == '#'
+                ) {
+                    occupiedAdjacentSeats++
+                }
+            }
+        }
+        return occupiedAdjacentSeats
+    }
+
+}
---
+++ src/test/java/se/johannesdahlgren/adventofcode2020/Day11Test.kt
@@ -0,0 +1,21 @@
+package se.johannesdahlgren.adventofcode2020
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class Day11Test {
+
+    @Test
+    fun part1Example() {
+        val day11 = Day11()
+        val occupiedSeats = day11.getOccupiedSeatsWhenStable(readFileTo2DArray("day11Example"))
+        assertEquals(37, occupiedSeats)
+    }
+
+    @Test
+    fun part1() {
+        val day11 = Day11()
+        val occupiedSeats = day11.getOccupiedSeatsWhenStable(readFileTo2DArray("day11"))
+        assertEquals(2263, occupiedSeats)
+    }
+}
