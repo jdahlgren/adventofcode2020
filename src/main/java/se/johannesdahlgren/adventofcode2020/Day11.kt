32f78e81d57845d746e21c01591b1857a5e64906
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2020-12-12 19:40:46 +0100

    Day11 part2 solved

--- src/main/java/se/johannesdahlgren/adventofcode2020/Day11.kt
+++ src/main/java/se/johannesdahlgren/adventofcode2020/Day11.kt
@@ -50,4 +50,70 @@ class Day11 {
         return occupiedAdjacentSeats
     }
 
+    fun getOccupiedVisibleSeatsWhenStable(input: List<List<Char>>): Int {
+        var seatMap = input.toList()
+        var seatsChanged = -1
+
+        while (seatsChanged != 0) {
+            seatsChanged = 0
+            val nextSeatMap = seatMap.map { it.toMutableList() }.toList()
+            for (rowIndex in seatMap.indices) {
+                for (colIndex in seatMap[rowIndex].indices) {
+                    val currentSeat = seatMap[rowIndex][colIndex]
+//                    print(currentSeat)
+                    val occupiedVisibleSeats = occupiedVisibleSeats(seatMap, rowIndex, colIndex)
+                    when {
+                        currentSeat == 'L' && occupiedVisibleSeats == 0 -> {
+                            nextSeatMap[rowIndex][colIndex] = '#'
+                            seatsChanged++
+                        }
+                        currentSeat == '#' && occupiedVisibleSeats >= 5 -> {
+                            nextSeatMap[rowIndex][colIndex] = 'L'
+                            seatsChanged++
+                        }
+                    }
+                }
+//                println()
+            }
+            seatMap = nextSeatMap
+//            println()
+        }
+        return seatMap.flatten().count { it == '#' }
+    }
+
+    private fun occupiedVisibleSeats(seatMap: List<List<Char>>, currentRow: Int, currentCol: Int): Int {
+        var occupiedVisibleSeats = 0
+        occupiedVisibleSeats += getOccupiedVisibleSeatsInDirection(-1, -1, currentRow, currentCol, seatMap)
+        occupiedVisibleSeats += getOccupiedVisibleSeatsInDirection(-1, 0, currentRow, currentCol, seatMap)
+        occupiedVisibleSeats += getOccupiedVisibleSeatsInDirection(-1, 1, currentRow, currentCol, seatMap)
+        occupiedVisibleSeats += getOccupiedVisibleSeatsInDirection(0, -1, currentRow, currentCol, seatMap)
+        occupiedVisibleSeats += getOccupiedVisibleSeatsInDirection(0, 1, currentRow, currentCol, seatMap)
+        occupiedVisibleSeats += getOccupiedVisibleSeatsInDirection(1, -1, currentRow, currentCol, seatMap)
+        occupiedVisibleSeats += getOccupiedVisibleSeatsInDirection(1, 0, currentRow, currentCol, seatMap)
+        occupiedVisibleSeats += getOccupiedVisibleSeatsInDirection(1, 1, currentRow, currentCol, seatMap)
+        return occupiedVisibleSeats
+    }
+
+    private fun getOccupiedVisibleSeatsInDirection(
+        rowDirection: Int,
+        colDirection: Int,
+        currentRow: Int,
+        currentCol: Int,
+        seatMap: List<List<Char>>
+    ): Int {
+        var nextRow = currentRow + rowDirection
+        var nextCol = currentCol + colDirection
+        while (nextRow in seatMap.indices && nextCol in seatMap[currentRow].indices ) {
+            if(seatMap[nextRow][nextCol] == 'L') {
+                return 0
+            } else if(seatMap[nextRow][nextCol] == '#') {
+                return 1
+            }
+            nextRow += rowDirection
+            nextCol += colDirection
+        }
+        return 0
+    }
+
+
 }
--- src/test/java/se/johannesdahlgren/adventofcode2020/Day11Test.kt
+++ src/test/java/se/johannesdahlgren/adventofcode2020/Day11Test.kt
@@ -18,4 +18,18 @@ internal class Day11Test {
         val occupiedSeats = day11.getOccupiedSeatsWhenStable(readFileTo2DArray("day11"))
         assertEquals(2263, occupiedSeats)
     }
+
+    @Test
+    fun part2Example() {
+        val day11 = Day11()
+        val occupiedSeats = day11.getOccupiedVisibleSeatsWhenStable(readFileTo2DArray("day11Example"))
+        assertEquals(26, occupiedSeats)
+    }
+
+    @Test
+    fun part2() {
+        val day11 = Day11()
+        val occupiedSeats = day11.getOccupiedVisibleSeatsWhenStable(readFileTo2DArray("day11"))
+        assertEquals(2002, occupiedSeats)
+    }
 }
